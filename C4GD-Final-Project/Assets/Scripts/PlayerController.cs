87f9b3d015797802d103124884109105aef383de
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-07-19 14:48:20 -0400

    Adding damage variable to player, fixing text positioning for main menu and controls when ratio is 16:9

--- C4GD-Final-Project/Assets/Scripts/PlayerController.cs
+++ C4GD-Final-Project/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 6f;
     public float jumpSpeed = 12f;
     public float dashSpeed = 16f;
+    public int damage = 5;
     private bool isDashing = false;
     private bool isAttacking = false;
     private float dashCd = 0;
@@ -129,7 +130,7 @@ public class PlayerController : MonoBehaviour
 
         foreach (Collider2D target in hitTargets)
         {
-            target.GetComponent<Enemy>().TakeDamage(4);
+            target.GetComponent<Enemy>().TakeDamage(damage);
         }
         //Time should be as long as the attack animation
         isAttacking = true;
