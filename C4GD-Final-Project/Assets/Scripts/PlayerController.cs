564909fe8c830f026971f991ab14f918def2af4a
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-07-20 11:09:56 -0400

    Adding WASD for dash direction

--- C4GD-Final-Project/Assets/Scripts/PlayerController.cs
+++ C4GD-Final-Project/Assets/Scripts/PlayerController.cs
@@ -108,19 +108,19 @@ public class PlayerController : MonoBehaviour
     private Vector2 GetDirection()
     {
         Vector2 result = Vector2.zero;
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))
         {
             result += new Vector2(1, 0);
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
         {
             result += new Vector2(-1, 0);
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w"))
         {
             result += new Vector2(0, 1);
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s"))
         {
             result += new Vector2(0, -1);
         }
