f58027b481cd41e0c46b22396e4a4bf8f990b996
ShinjiK1 <92060952+ShinjiK1@users.noreply.github.com>
2022-07-20 11:18:36 -0400

    Fixed attack

--- C4GD-Final-Project/Assets/Scripts/PlayerController.cs
+++ C4GD-Final-Project/Assets/Scripts/PlayerController.cs
@@ -68,10 +68,10 @@ public class PlayerController : MonoBehaviour
             dashCd = 1.5f;
             StartCoroutine(Dash(GetDirection()));
         }
-        if (Input.GetKey("x") && !isDashing && Time.deltaTime >= nextAttackTime)
+        if (Input.GetKey("x") && !isDashing && Time.time >= nextAttackTime)
         {
             StartCoroutine(Attack(GetDirection()));
-            nextAttackTime = Time.deltaTime + attackCd;
+            nextAttackTime = Time.time + attackCd;
         }
     }
 
@@ -108,19 +108,19 @@ public class PlayerController : MonoBehaviour
     private Vector2 GetDirection()
     {
         Vector2 result = Vector2.zero;
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))
         {
             result += new Vector2(1, 0);
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
         {
             result += new Vector2(-1, 0);
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w"))
         {
             result += new Vector2(0, 1);
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s"))
         {
             result += new Vector2(0, -1);
         }
