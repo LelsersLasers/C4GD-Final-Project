47e8271efb5b31e270e90a371f4a03d205b803ea
Lord-Lelsers <millan.kumar.24@bishops.com>
2022-07-20 14:33:08 -0400

    Improving platforms

--- C4GD-Final-Project/Assets/Scripts/PlayerController.cs
+++ C4GD-Final-Project/Assets/Scripts/PlayerController.cs
@@ -240,7 +240,11 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if ((collision.gameObject.tag == "Ground") || (collision.gameObject.tag == "Platform" && collision.gameObject.GetComponent<BoxCollider2D>().enabled))
+        if ((collision.gameObject.tag == "Ground"))
+        {
+            isOnGround = true;
+        }
+        if (collision.gameObject.tag == "Platform" && collision.gameObject.GetComponent<BoxCollider2D>().enabled)
         {
             isOnGround = true;
         }
