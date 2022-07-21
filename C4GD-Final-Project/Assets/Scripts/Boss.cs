6cdd0e0e6e4c12059511f36d4d6a0501601bd1cb
ShinjiK1 <92060952+ShinjiK1@users.noreply.github.com>
2022-07-21 13:24:00 -0400

    Made some boss attacks

--- C4GD-Final-Project/Assets/Scripts/Boss.cs
+++ C4GD-Final-Project/Assets/Scripts/Boss.cs
@@ -8,6 +8,13 @@ public class Boss : MonoBehaviour
     private Animator bossAnim;
     private SpriteRenderer bossSR;
     private GameObject player;
+    public float attackCd;
+    public GameObject bossFireball1;
+    public GameObject bossFireball2;
+    private float timeSinceAttack = 0f;
+    public float leftBound;
+    public float rightBound;
+    private int attackChooser = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +27,48 @@ public class Boss : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.transform.position.x > leftBound && player.transform.position.x < rightBound)
+        {
+            transform.position = new Vector3(player.transform.position.x, transform.position.y, 0);
+        }
+        if (Time.time > timeSinceAttack)
+        {
+            attackChooser = Random.Range(0, 2);
+            if (attackChooser == 0)
+            {
+                AttackOne();
+            }
+            else if (attackChooser == 1)
+            {
+                AttackTwo();
+            }
+            timeSinceAttack = Time.time + attackCd;
+        }
+    }
+
+    //Shoots a spread of fireballs that target the player
+    void AttackOne()
+    {
+        for (int i = -2; i < 3; i++)
+        {
+            Instantiate(bossFireball1, transform.position + new Vector3(2 * i, -1), Quaternion.FromToRotation(Vector2.right, player.transform.position));
+        }
+    }
+
+    //Summons fireballs from the left and right of the screen 
+    void AttackTwo()
+    {
+        int leftOrRight = (Random.Range(1, 3) * 2) - 3;
+        int yOffset = 0;
+        //If the player is around less than 2 blocks above the ground
+        if (player.transform.position.y < -1)
+        {
+            yOffset = 4;
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(bossFireball2, player.transform.position + new Vector3(12 * leftOrRight, player.transform.position.y + yOffset + i, 0), bossFireball2.transform.rotation);
+            Instantiate(bossFireball2, player.transform.position + new Vector3(-12 * leftOrRight, player.transform.position.y + yOffset - i, 0), bossFireball2.transform.rotation);
+        }
     }
 }
