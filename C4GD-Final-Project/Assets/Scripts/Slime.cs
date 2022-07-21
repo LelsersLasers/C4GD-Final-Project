7089ba54757d5e34a813241f767a79158d1b0d2a
ShinjiK1 <92060952+ShinjiK1@users.noreply.github.com>
2022-07-21 10:59:29 -0400

    Made player object private and accessed in start methods

--- C4GD-Final-Project/Assets/Scripts/Slime.cs
+++ C4GD-Final-Project/Assets/Scripts/Slime.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Slime : MonoBehaviour
 {
-    public GameObject player;
+    private GameObject player;
     private bool lockedOn;
     public float aggroRange;
     public float minJump;
@@ -22,6 +22,7 @@ public class Slime : MonoBehaviour
     {
         slimeRb = GetComponent<Rigidbody2D>();
         slimeAnim = GetComponent<Animator>();
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
