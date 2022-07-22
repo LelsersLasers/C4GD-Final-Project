1ce14104aaa8cbfe0bcadb63c67e5ae80fe1bc45
TopoNeko <66758943+TopoNeko@users.noreply.github.com>
2022-07-22 10:55:30 -0400

    Arrows

--- C4GD-Final-Project/Assets/Pointer.cs
+++ C4GD-Final-Project/Assets/Pointer.cs
@@ -5,41 +5,22 @@ using UnityEngine;
 public class Pointer : MonoBehaviour
 
 {
-    private GameObject player;
-    public SpriteRenderer Up, RightUp, Right,RightDown,Down,LeftDown,Left,LeftUp;
-    private float orientation = 1f;
-    private Rigidbody2D Rightrb;
-    private SpriteRenderer sr;
-    private GameObject RightUpV;
+    public PlayerController player;
+    public GameObject Up, RightUp, Right,RightDown,Down,LeftDown,Left,LeftUp;
     
 
 
     // Start is called before the first frame update
     void Start()
     {
-        RightUpV = GameObject.Find("RightUp");
-        Rightrb = Right.GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(orientation);
-        if (orientation == 1)
-        {
-            RightUp.flipX = false;
-            Right.flipX = false;
-            RightDown.flipX = false;
-        }
-        else if (orientation == -1)
-        {
-            RightUp.flipX = true;
-            //Vector3 SwapPos = new Vector3 (RightUpV.transform.position.x,RightUpV.transform.position.y,RightUpV.transform.rotation.z + 90);
-            RightUpV += (0,0,RightUpV.transform.rotation.z + 90);
-            Right.flipX = true;
-            RightDown.flipX = true;
-        }
-        StartCoroutine(Point(GetDirection()));
+        Debug.Log(player.orientation);
+        Point(GetDirection());
        
     }
     private Vector2 GetDirection()
@@ -48,14 +29,10 @@ public class Pointer : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))
         {
             result += new Vector2(1, 0);
-            orientation = 1;
-
         }
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
         {
             result += new Vector2(-1, 0);
-            orientation = -1;
-
         }
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w"))
         {
@@ -67,35 +44,54 @@ public class Pointer : MonoBehaviour
         }
         if (result == Vector2.zero)
         {
-            return transform.right * orientation;
+            return transform.right * player.orientation;
         }
-        Debug.Log(result);
         return result.normalized;
- 
     }
-    private IEnumerator Point(Vector2 direction)
+    private void Point(Vector2 direction)
     {
+        RightUp.SetActive(false);
+        LeftUp.SetActive(false);
+        Right.SetActive(false);
+        Left.SetActive(false);
+        RightDown.SetActive(false);
+        LeftDown.SetActive(false);   
+        Up.SetActive(false);
+        Down.SetActive(false);        
+
+
         Debug.Log("working");
-          if (direction == new Vector2(1, 1).normalized || direction == new Vector2(-1, 1).normalized)
+        if (direction == new Vector2(1, 1).normalized)
         {
-            
+            RightUp.SetActive(true);
+        }  
+        else if (direction == new Vector2(-1, 1).normalized) {
+            LeftUp.SetActive(true);
         }
-        else if (direction == new Vector2(1, 0).normalized || direction == new Vector2(-1, 0).normalized)
+        else if (direction == new Vector2(1, 0).normalized ) 
         {
-            
+        Right.SetActive(true);
         }
-        else if (direction == new Vector2(1, -1).normalized || direction == new Vector2(-1, -1).normalized)
+        else if (direction == new Vector2(-1, 0).normalized)
         {
-            
+        Left.SetActive(true);
+        }
+        else if (direction == new Vector2(1, -1).normalized)
+        {
+        RightDown.SetActive(true);
+        }
+        else if(direction == new Vector2(-1, -1).normalized)
+        {
+         LeftDown.SetActive(true);   
         }
         else if (direction == new Vector2(0, 1).normalized)
         {
-           
+           Up.SetActive(true);
         }
         else if (direction == new Vector2(0, -1).normalized)
         {
-
-        }
-        yield return new WaitForSeconds(0f);
+            Down.SetActive(true);        }
     }
+
+
 }
